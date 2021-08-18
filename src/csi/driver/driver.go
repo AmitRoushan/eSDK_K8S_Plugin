683c3215e440ae02270da6310b5e6f3c2427298a
Amit Roushan <amit.roushan@huawei.com>
2021-08-18 19:06:29 +0530

    add topology awareness for node plugin

--- src/csi/driver/driver.go
+++ src/csi/driver/driver.go
@@ -1,17 +1,26 @@
 package driver
 
+import(
+	"utils/k8sutils"
+)
+
 type Driver struct {
 	name            string
 	version         string
 	useMultiPath    bool
 	isNeedMultiPath bool
+	k8sUtils        k8sutils.Interface
+	nodeName 		string
 }
 
-func NewDriver(name, version string, useMultiPath, isNeedMultiPath bool) *Driver {
+func NewDriver(name, version string, useMultiPath, isNeedMultiPath bool,
+				k8sUtils k8sutils.Interface, nodeName string) *Driver {
 	return &Driver{
 		name:            name,
 		version:         version,
 		useMultiPath:    useMultiPath,
 		isNeedMultiPath: isNeedMultiPath,
+		k8sUtils:		k8sUtils,
+		nodeName: 		nodeName,
 	}
 }
